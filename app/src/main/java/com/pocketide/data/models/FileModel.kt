989562002d62d59295a7a9f70287ab6defad85b0
adiweb12 <vsadith051@gmail.com>
2026-03-04 13:21:43 +0530

    Delete app directory

--- app/src/main/java/com/pocketide/data/models/FileModel.kt
+++
@@ -1,9 +0,0 @@
-package com.pocketide.data.models
-
-data class FileModel(
-    val name: String,
-    val content: String,
-    val language: String,
-    val path: String,
-    val isDirty: Boolean = false
-)
--- app/src/main/java/com/pocketide/data/models/PluginModel.kt
+++
@@ -1,9 +0,0 @@
-package com.pocketide.data.models
-
-data class PluginModel(
-    val name: String,
-    val version: String,
-    val language: String,
-    val snippetsPath: String,
-    val enabled: Boolean = true
-)
